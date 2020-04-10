39a10fcca801224ca9ec49f3a8a18b503de60562
varalakshmi-iOS <varalakshmi1318@gmail.com>
2020-04-10 13:55:22 +0530

    added func

--- Tech Trial/LoginVC.swift
+++ Tech Trial/LoginVC.swift
@@ -12,9 +12,21 @@ class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        testBranchMerging()
+        
         // Do any additional setup after loading the view.
     }
 
-
+    func testBranchMerging() {
+        let a = 1
+        let b = 2
+        
+        let c = a + b
+        
+        print(c)
+    }
+    
+    
 }
 
